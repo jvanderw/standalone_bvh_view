b597a7751073faf378587298420981b6d6431ed7
Jess VanDerwalker <jess@apdm.com>
2018-08-21 12:21:59 -0700

    Refresh on loading new file.

--- js/standalone_bvh.js
+++ js/standalone_bvh.js
@@ -6,7 +6,19 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
     alert('The File APIs are not fully supported in this browser.');
 }
 
+var _renderer = null;
 
+function getFiles(files) {
+    document.getElementById('current-file-name')
+        .textContent = files[0].name;
+    clearScene();
+    var reader = new FileReader();
+    reader.onload = function () {
+        animateBvh(this.result);
+    };
+    reader.readAsText(files[0]);
+
+}
 
 function animateBvh(bvhFile) {
 
@@ -19,45 +31,50 @@ function animateBvh(bvhFile) {
 
     var loader = new THREE.BVHLoader();
 
-    loader.load( "../data/20180816-150805_Free_Form_Sprint_and_Walk_Analysis.h5.bvh", function( result ) {
-        skeletonHelper = new THREE.SkeletonHelper( result.skeleton.bones[ 0 ] );
-        skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to SkeletonHelper directly
-        var boneContainer = new THREE.Group();
-        boneContainer.add( result.skeleton.bones[ 0 ] );
-        scene.add( skeletonHelper );
-        scene.add( boneContainer );
-        // play animation
-        mixer = new THREE.AnimationMixer( skeletonHelper );
-        mixer.clipAction( result.clip ).setEffectiveWeight( 1.0 ).play();
-    } );
+    var result = loader.parse(bvhFile);
+    skeletonHelper = new THREE.SkeletonHelper( result.skeleton.bones[ 0 ] );
+    skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to SkeletonHelper directly
+    var boneContainer = new THREE.Group();
+    boneContainer.add( result.skeleton.bones[ 0 ] );
+    scene.add( skeletonHelper );
+    scene.add( boneContainer );
+    // play animation
+    mixer = new THREE.AnimationMixer( skeletonHelper );
+    mixer.clipAction( result.clip ).setEffectiveWeight( 1.0 ).play();
 
     function init() {
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 500 );
-        camera.position.set( 0, 100, 200 );
+        camera.position.set( 25, 25, 25 );
         controls = new THREE.OrbitControls( camera );
         controls.minDistance = 10;
         controls.maxDistance = 400;
         scene = new THREE.Scene();
         scene.background = new THREE.Color( 0xeeeeee );
-        scene.add( new THREE.GridHelper( 100, 10 ) );
+        scene.add( new THREE.GridHelper( 20, 10 ) );
         // renderer
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
+        _renderer = new THREE.WebGLRenderer( { antialias: true } );
+        _renderer.setPixelRatio( window.devicePixelRatio );
+        _renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( _renderer.domElement );
         window.addEventListener( 'resize', onWindowResize, false );
     }
 
     function onWindowResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        _renderer.setSize( window.innerWidth, window.innerHeight );
     }
 
     function animate() {
         requestAnimationFrame( animate );
         var delta = clock.getDelta();
         if ( mixer ) mixer.update( delta );
-        renderer.render( scene, camera );
+        _renderer.render( scene, camera );
+    }
+}
+
+function clearScene() {
+    if (_renderer) {
+        _renderer.domElement.remove();
     }
 }
